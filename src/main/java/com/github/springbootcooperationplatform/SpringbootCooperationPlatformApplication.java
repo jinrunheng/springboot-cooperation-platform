21be584a76099319012404f71cbfb28f0d207c5f
jinrunheng <1175088275@qq.com>
2021-05-23 18:21:12 +0800

    init springboot-cooperation-platform project

---
+++ src/main/java/com/github/springbootcooperationplatform/SpringbootCooperationPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.github.springbootcooperationplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCooperationPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootCooperationPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/springbootcooperationplatform/SpringbootCooperationPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.springbootcooperationplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCooperationPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
