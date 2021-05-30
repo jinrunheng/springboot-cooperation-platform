c4658f1b27f8cf5dd0111b3bc9a2426cee95df7f
jinrunheng <1175088275@qq.com>
2021-05-31 02:07:45 +0800

    finish test testEmptyBlogReturnSystemError & testUserRegisterSucceedAndLogin

--- src/main/resources/db/migration/V1__Create_User.sql
+++ src/main/resources/db/migration/V1__Create_User.sql
@@ -3,7 +3,7 @@ create table user
     id                 bigint primary key auto_increment,
     username           varchar(255),
     encrypted_password varchar(255),
-    avatar             varchar(2000) default '_',
+    avatar             varchar(5000) default '_',
     created_at         datetime,
     updated_at         datetime
 )
--- src/test/java/com/github/springbootcooperationplatform/integration/IntegrationTest.java
+++ src/test/java/com/github/springbootcooperationplatform/integration/IntegrationTest.java
@@ -1,6 +1,5 @@
 package com.github.springbootcooperationplatform.integration;
 
-import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.github.springbootcooperationplatform.SpringbootCooperationPlatformApplication;
 import okhttp3.*;
@@ -25,10 +24,14 @@ public class IntegrationTest {
     @Autowired
     Environment environment;
 
+    private String getUrl(String path) {
+        String port = environment.getProperty("local.server.port");
+        return "http://localhost:" + port + path;
+    }
+
     @Test
     public void notLoggedInByDefault() {
-        String port = environment.getProperty("local.server.port");
-        String url = "http://localhost:" + port + "/auth";
+        String url = getUrl("/auth");
         OkHttpClient httpClient = new OkHttpClient();
         Request request = new Request.Builder()
                 .url(url)
@@ -46,8 +49,7 @@ public class IntegrationTest {
     @Test
     public void registerSucceed() {
         final MediaType JSON = MediaType.get("application/json;charset=utf-8");
-        String port = environment.getProperty("local.server.port");
-        String url = "http://localhost:" + port + "/auth/register";
+        String url = getUrl("/auth/register");
         OkHttpClient httpClient = new OkHttpClient();
         JSONObject testJson = new JSONObject();
         testJson.put("username", "testUser");
@@ -64,6 +66,54 @@ public class IntegrationTest {
             Assertions.assertFalse(Objects.isNull(map.get("data")));
         } catch (IOException e) {
         }
+    }
+
+    @Test
+    public void testUserRegisterSucceedAndLogin() {
+        final MediaType JSON = MediaType.get("application/json;charset=utf-8");
+        OkHttpClient httpClient = new OkHttpClient();
+        JSONObject testJson = new JSONObject();
+        testJson.put("username", "test");
+        testJson.put("password", "123456");
+        RequestBody requestBody = RequestBody.create(JSON, testJson.toString());
+        Request registerRequest = new Request.Builder()
+                .url(getUrl("/auth/register"))
+                .post(requestBody)
+                .build();
+
+        Request loginRequest = new Request.Builder()
+                .url(getUrl("/auth/login"))
+                .post(requestBody)
+                .build();
+
+
+        try {
+            httpClient.newCall(registerRequest).execute();
+            Response loginResponse = httpClient.newCall(loginRequest).execute();
+            Map<String, Object> loginMap = JSONObject.parseObject(loginResponse.body().string());
+            Assertions.assertEquals(loginMap.get("status"), "ok");
+            Assertions.assertFalse(Objects.isNull(loginMap.get("data")));
+
+        } catch (IOException e) {
+        }
+
+    }
+
+    @Test
+    public void testEmptyBlogReturnSystemError() {
+        OkHttpClient httpClient = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(getUrl("/blog/1"))
+                .build();
+        Response response = null;
+        try {
+            response = httpClient.newCall(request).execute();
+            Map<String, Object> map = JSONObject.parseObject(response.body().string());
+            Assertions.assertEquals(map.get("status"), "fail");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
 }
